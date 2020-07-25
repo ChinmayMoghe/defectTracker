3e76f20a622ecf50d35863199cc80e721ad638f9
chinmay <chinmay11.moghe@gmail.com>
2020-07-25 13:59:47 +0530

    Router changes for the app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 import LoginComponent from './components/LoginComponent';
 import AddDefectComponent from './components/AddDefectComponent';
 import ViewComponent from './components/ViewDefectComponent';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 const App = () => {
     return (
         <div className="app-container">
             <div className="app-header"><h1>DEFECT TRACKER</h1></div>
             <div className="app-body">
-                {/* <LoginComponent /> */}
-                {/* <AddDefectComponent /> */}
-                <ViewComponent />
+                <Switch>
+                    <Route path='/viewdefect'>
+                        <ViewComponent/>
+                    </Route>
+                    <Route path='/adddefect'>
+                        <AddDefectComponent/>
+                    </Route>
+                    <Route path='/login'>
+                        <LoginComponent/>
+                    </Route>
+                    <Route path='/'>
+                        <LoginComponent/>
+                    </Route>
+                </Switch>
             </div>
         </div>
     );
